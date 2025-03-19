b5b32bc77c0128be9e5749914aae7a0d4fb029d9
AnugrahFadillah <161240712+AnugrahFadillah@users.noreply.github.com>
2025-03-19 23:10:07 +0700

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize('notes', 'root', '', {
-    host: "127.0.0.1",
+const db = new Sequelize('RECOVER_YOUR_DATA', 'root', '', {
+    host: "34.60.66.67",
     dialect: "mysql"
 });
 
